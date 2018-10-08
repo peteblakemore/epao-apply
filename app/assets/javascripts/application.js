cd4f70d66ad921e32dd128792880bb126cfb5588
Greg Barnes <gregdbarnes@gmail.com>
2018-10-08 10:38:32 +0100

    Add hide/show password and 'send another email' link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,14 +2,14 @@
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production');
+  window.console.info("GOV.UK Prototype Kit - do not use for production");
 }
 
 $(document).ready(function() {
-  $('.js-prerequisites').hide();
-  $('#standard-name').autocomplete({
+  $(".js-prerequisites").hide();
+  $("#standard-name").autocomplete({
     source: function(request, response) {
-      $.getJSON('/public/javascripts/standards.json', function(data) {
+      $.getJSON("/public/javascripts/standards.json", function(data) {
         var array = $.map(data, function(standard) {
           const { value, name, requirements } = standard;
           return {
@@ -23,16 +23,16 @@ $(document).ready(function() {
       });
     },
     select: function(event, ui) {
-      $('.js-standard-name').text(ui.item.name);
-      $('.js-prerequisites ul').empty();
+      $(".js-standard-name").text(ui.item.name);
+      $(".js-prerequisites ul").empty();
       if (ui.item.requirements) {
         ui.item.requirements.map(function(requirement, index) {
-          $('.js-prerequisites ul').append('<li>' + requirement + '</li>');
+          $(".js-prerequisites ul").append("<li>" + requirement + "</li>");
         });
-        $('.js-prerequisites').show();
+        $(".js-prerequisites").show();
       } else {
-        $('.js-prerequisites ul').empty();
-        $('.js-prerequisites').hide();
+        $(".js-prerequisites ul").empty();
+        $(".js-prerequisites").hide();
       }
       // Clear input on select.. if we do this
       // (or need to carry any other data accross) we'll need to user hidden form inputs
@@ -41,33 +41,65 @@ $(document).ready(function() {
     }
   });
 
-  if (GOVUK.cookie('reviewed')) {
-    $('.js-send-for-review-clicked').show();
-    $('.js-send-for-review-not-clicked').hide();
+  if (GOVUK.cookie("reviewed")) {
+    $(".js-send-for-review-clicked").show();
+    $(".js-send-for-review-not-clicked").hide();
   } else {
-    $('.js-send-for-review-clicked').hide();
-    $('.js-send-for-review-not-clicked').show();
+    $(".js-send-for-review-clicked").hide();
+    $(".js-send-for-review-not-clicked").show();
   }
 
-  $('.js-review-passed').on('click', function(event) {
+  $(".js-review-passed").on("click", function(event) {
     // event.preventDefault();
-    GOVUK.cookie('reviewed', true, { days: 30 });
+    GOVUK.cookie("reviewed", true, { days: 30 });
     // $(this).hide();
-    $('.js-send-for-review-clicked').show();
-    $('.js-send-for-review-not-clicked').hide();
+    $(".js-send-for-review-clicked").show();
+    $(".js-send-for-review-not-clicked").hide();
   });
 
-  $('.js-clone-director').on('click', function(event) {
+  $(".js-clone-director").on("click", function(event) {
     event.preventDefault();
-    $('.js-director-markup')
+    $(".js-director-markup")
       .clone()
-      .insertAfter('.js-director-markup')
-      .removeClass('js-director-markup');
+      .insertAfter(".js-director-markup")
+      .removeClass("js-director-markup");
 
     // var $clone = $('.js-director-markup:last').clone();
     // $clone.find('input');
   });
 });
+
+(function() {
+  var PasswordToggler = function(element, field) {
+    this.element = element;
+    this.field = field;
+    this.toggle();
+  };
+
+  PasswordToggler.prototype = {
+    toggle: function() {
+      var self = this;
+      self.element.addEventListener(
+        "change",
+        function() {
+          if (self.element.checked) {
+            self.field.setAttribute("type", "text");
+          } else {
+            self.field.setAttribute("type", "password");
+          }
+        },
+        false
+      );
+    }
+  };
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var checkbox = document.querySelector("#show-password"),
+      pwd = document.querySelector("#new-password"),
+
+    var toggler = new PasswordToggler(checkbox, pwd);
+  });
+})();
 
 // $clone = $('.item-name:last').clone();
 // for (i = 1; i < 999; i++) {
--- app/routes.js
+++ app/routes.js
@@ -1,82 +1,83 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-require('dotenv').config();
+require("dotenv").config();
 
 // gov.uk notify setup
 // https://github.com/alphagov/govuk-prototype-kit/blob/61c50892ab74c1d6290c5b776107327c31695fca/docs/documentation/using-notify.md
-const NotifyClient = require('notifications-node-client').NotifyClient;
+const NotifyClient = require("notifications-node-client").NotifyClient;
 const notify = new NotifyClient(process.env.NOTIFYAPIKEY);
 
 // Route index page
-router.get('/', function(req, res) {
-  res.render('index');
+router.get("/", function(req, res) {
+  res.render("index");
 });
 
 // Add your routes here - above the module.exports line
 
 // Send out email at start of journey with code to paste into website
-router.post('/0-eligibility/setup', (req, res) => {
-  if (!req.body.email) {
-    res.send('Email is required');
+router.post("/send-setup-email", (req, res) => {
+  const email = req.body.email || req.session.data["email"];
+  if (!email) {
+    res.send("Email is required");
   } else {
     notify
-      .sendEmail('f3fcdcb6-dd32-4d63-81a5-992164a74d14', req.body.email)
-      .then(response => console.log('Account setup email sent'))
+      .sendEmail("f3fcdcb6-dd32-4d63-81a5-992164a74d14", email)
+      .then(response => console.log("Account setup email sent"))
       .catch(err => console.error(err));
-    res.redirect('/0-eligibility/confirm-identity');
+    res.redirect("/0-eligibility/confirm-identity");
   }
 });
 
-router.post('/review-complete', (req, res) => {
-  if (!req.session.data['email']) {
-    res.send('Email required');
+router.post("/review-complete", (req, res) => {
+  if (!req.session.data["email"]) {
+    res.send("Email required");
   } else {
     notify
       .sendEmail(
-        '0d9cc7a4-e06b-4f7d-9c0b-21f746eb9f56',
-        req.session.data['email']
+        "0d9cc7a4-e06b-4f7d-9c0b-21f746eb9f56",
+        req.session.data["email"]
       )
-      .then(response => console.log('Failed application email sent'))
+      .then(response => console.log("Failed application email sent"))
       .catch(err => console.error(err));
-    res.redirect('/application-complete');
+    res.redirect("/application-complete");
   }
 });
 
 // Send out email after section 4 has been competed (Submit application button clicked) with link to return to recieve feedback
-router.post('/application-complete', (req, res) => {
-  if (!req.session.data['standard-name'] || !req.session.data['email']) {
-    res.send('Email and standard have not been completed');
+router.post("/application-complete", (req, res) => {
+  if (!req.session.data["standard-name"] || !req.session.data["email"]) {
+    res.send("Email and standard have not been completed");
   } else {
     notify
       .sendEmail(
-        'e19c06cb-eb30-4a22-91b4-a3638a364583',
-        req.session.data['email'],
+        "e19c06cb-eb30-4a22-91b4-a3638a364583",
+        req.session.data["email"],
         {
-          standard: req.session.data['standard-name']
+          standard: req.session.data["standard-name"]
         }
       )
-      .then(response => console.log('Failed application email sent'))
+      .then(response => console.log("Failed application email sent"))
       .catch(err => console.error(err));
-    res.redirect('/application-complete');
+    res.redirect("/application-complete");
   }
 });
 
 // Send out email after feedback questions have been competed (Re-submit application button clicked) with link to return to application successful page
-router.post('/re-submit-application', (req, res) => {
-  if (!req.session.data['standard-name'] || !req.session.data['email']) {
-    res.send('Email and standard have not been completed');
+router.post("/re-submit-application", (req, res) => {
+  if (!req.session.data["standard-name"] || !req.session.data["email"]) {
+    res.send("Email and standard have not been completed");
   } else {
     notify
       .sendEmail(
-        '62d2febd-cb2f-4dc2-9cd8-180a082a2b89',
-        req.session.data['email'],
+        "62d2febd-cb2f-4dc2-9cd8-180a082a2b89",
+        req.session.data["email"],
         {
-          standard: req.session.data['standard-name']
+          standard: req.session.data["standard-name"]
         }
       )
-      .then(response => console.log('Failed application email sent'))
+      .then(response => console.log("Failed application email sent"))
       .catch(err => console.error(err));
-    res.redirect('/application-complete');
+    res.redirect("/application-complete");
   }
 });
 
